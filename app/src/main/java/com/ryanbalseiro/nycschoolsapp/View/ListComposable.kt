5101aace66fd9b8cc695b7fd086c252bf0bac6e3
Ryan <balseiro1029@gmail.com>
2024-12-04 15:58:14 -0500

    add icon, remove unused composable

--- app/src/main/java/com/ryanbalseiro/nycschoolsapp/View/ListComposable.kt
+++
@@ -1,9 +0,0 @@
-package com.ryanbalseiro.nycschoolsapp.View
-
-import androidx.compose.foundation.lazy.LazyListScope
-import androidx.compose.runtime.Composable
-
-@Composable
-fun ListComposable() {
-
-}
