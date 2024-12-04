822983ef5323471fe7d8c953a1372fed37188999
Ryan <balseiro1029@gmail.com>
2024-12-04 15:57:50 -0500

    Api logic, updated data classes

--- app/src/main/java/com/ryanbalseiro/nycschoolsapp/Model/ApiService.kt
+++ app/src/main/java/com/ryanbalseiro/nycschoolsapp/Model/ApiService.kt
@@ -3,9 +3,20 @@ package com.ryanbalseiro.nycschoolsapp.Model
 import retrofit2.Retrofit
 import retrofit2.http.GET
 import retrofit2.converter.gson.GsonConverterFactory
+import okhttp3.OkHttpClient
+import java.util.concurrent.TimeUnit
 
-private val retrofit = Retrofit.Builder().baseUrl("https://data.cityofnewyork.us/resource/")
-    .addConverterFactory(GsonConverterFactory.create()).build()
+val okHttpClient = OkHttpClient.Builder()
+    .connectTimeout(15, TimeUnit.SECONDS) // Connect timeout
+    .readTimeout(30, TimeUnit.SECONDS)    // Read timeout
+    .writeTimeout(30, TimeUnit.SECONDS)   // Write timeout
+    .build()
+
+private val retrofit = Retrofit.Builder()
+    .baseUrl("https://data.cityofnewyork.us/resource/")
+    .client(okHttpClient) // Use the custom OkHttpClient
+    .addConverterFactory(GsonConverterFactory.create())
+    .build()
 
 val schoolAPIService = retrofit.create(ApiService::class.java)
 
--- app/src/main/java/com/ryanbalseiro/nycschoolsapp/Model/SATDataItem.kt
+++ app/src/main/java/com/ryanbalseiro/nycschoolsapp/Model/SATDataItem.kt
@@ -1,10 +1,14 @@
 package com.ryanbalseiro.nycschoolsapp.Model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class SATDataItem(
-    val dbn: String,
-    val num_of_sat_test_takers: String,
-    val sat_critical_reading_avg_score: String,
-    val sat_math_avg_score: String,
-    val sat_writing_avg_score: String,
-    val school_name: String
-)
+    val dbn: String?,
+    val num_of_sat_test_takers: String?,
+    val sat_critical_reading_avg_score: String?,
+    val sat_math_avg_score: String?,
+    val sat_writing_avg_score: String?,
+    val school_name: String?
+): Parcelable
